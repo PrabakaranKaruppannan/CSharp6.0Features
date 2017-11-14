830eab14d011a421cc47ccc7857ee0c264774427
Prabakaran, Karuppannan (GfK) <Karuppannan.Prabakaran@gfk.com>
2017-11-14 11:42:20 +0800

    Added Initial Projects

---
+++ CSharp6.0Features/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp6._0Features
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //CSharp6.0 Features
+            //1. ReadOnly Property
+            //2. 
+            //3.
+            //4.
+            //5.
+            //6.
+            //7.
+            //8.
+            //9.
+            //10.
+        }
+    }
+}
---
+++ CSharp6.0Features/ReadOnlyProperty.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace CSharp6._0Features
+{
+    class ReadOnlyProperty
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer();
+            customer.FirstName = "TestFirstName";
+
+            customer.ChangeLastName("TestLastName");
+
+            Console.WriteLine(customer.FirstName);
+            Console.WriteLine(customer.LastName);
+            Console.ReadLine();
+        }
+    }
+
+    class Customer
+    {
+        public string FirstName { get; set; }
+
+        // Before 6.0. But it cannot give full read only access. 
+        // You can modify the lastname using the below method "ChangeLastName";
+        public string LastName { get; private set; }
+
+        public void ChangeLastName(string lastName)
+        {
+            LastName = lastName;
+        }
+
+
+        // In 6.0. It gives full read only access. 
+        // You cannot modify the fullname with the below method "ChangeFullName". It gives compile error when you uncomment the code inside that method.
+        public string FullName { get; }
+
+
+        public void ChangeFullName(string lastName)
+        {
+            //FullName = lastName;
+        }
+    }
+}
